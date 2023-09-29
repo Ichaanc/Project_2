a90e35607f4860af0d49b098c2ce151b207f2d28
ichaanc <christiankylecapistani@gmail.com>
2023-09-29 16:35:26 +0800

    add

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-//  play and pause
-let play = document.querySelector(".play");
-let pause = document.querySelector(".pause");
-let audio = new Audio("sound/bgmusic.wav");
+bg("sound/bgmusic.wav");
 
-play.addEventListener("click", () => {
+function bg(audioName){
+  let audio = new Audio(audioName);
   audio.play();
-});
-pause.addEventListener("click", () => {
-  audio.pause();
-});
-//  play pause end
+}
